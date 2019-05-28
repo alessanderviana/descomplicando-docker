937b4b18112ad37bd368d477bf6716fdd3068114
alessanderviana <alessander.viana@hotmail.com>
2019-05-28 18:24:17 -0300

    Added the possibility to start more instances, a count

--- descomplicando-docker.tf
+++ descomplicando-docker.tf
@@ -29,16 +29,19 @@ provider "google" {
 
 // Linux instance - Ubuntu 16.04
 resource "google_compute_instance" "descomplicando-docker" {
- name         = "descomplicando-docker"
+ count = 3
+ name         = "descomplicando-docker-${count.index + 1}"
  # machine_type = "n1-standard-1"  # 3.75 GB RAM
  machine_type = "g1-small"  # 1.7 GB RAM
  zone         = "${var.region}-b"
 
- tags = [ "descomplicando-docker" ]
+ tags = [ "descomplicando-docker-${count.index + 1}" ]
 
  boot_disk {
    initialize_params {
-     image = "ubuntu-1604-xenial-v20190306"
+     # image = "ubuntu-1604-xenial-v20190306"
+     image = "ubuntu-minimal-1804-bionic-v20190514"
+     # image = "ubuntu-minimal-1904-disco-v20190528"
    }
  }
 
